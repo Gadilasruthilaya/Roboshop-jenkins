df3155d616b7c81a9a11062ff90e0acae73089e4
sruthilaya <sruthilayagadila@gmail.com>
2024-01-24 12:32:37 +0530

    cart

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,32 +1,29 @@
 def call() {
     node('workshop') {
 
-            if (env.cibuild == "java") {
-                stage('build') {
-                    sh 'mvn package'
-                    echo 'build'
-                }
+        // if (env.cibuild == "java") {
+        stage('build') {
+            sh 'mvn package'
+            echo 'build'
+        }
+    //}
             stage('unit test') {
                 echo 'unit test'
                 // sh 'npm test'
             }
             stage('code analysis') {
                 echo 'code analysis'
-                sh 'sonar-scanner -Dsonar.host.url=http://172.31.86.197:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
+               // sh 'sonar-scanner -Dsonar.host.url=http://172.31.86.197:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
 
             }
 
             stage('security scans') {
-
                 echo 'security scans'
-
             }
 
             stage('artifact creation') {
                 echo 'artifact creation'
-
             }
         }
 
     }
-}
